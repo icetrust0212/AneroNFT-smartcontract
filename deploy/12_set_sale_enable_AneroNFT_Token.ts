cdabc2ab52055e872baa167e86f2d4559c459c69
trust0212 <sergeyfedorenkodev@outlook.com>
2022-06-18 09:45:05 -0400

    contract

--- deploy/12_set_sale_enable_AneroNFT_Token.ts
+++ deploy/12_set_sale_enable_AneroNFT_Token.ts
@@ -1,19 +1,22 @@
 import { DeployFunction } from 'hardhat-deploy/types';
+import IERC20 from './interface/IERC20';
 
 const fn: DeployFunction = async function ({ deployments: { deploy, get, execute }, ethers: { getSigners, provider, utils }, network }) {
   const deployer = (await getSigners())[0];
-
+  const tAddress = "0xc7D4796B43272A329Ba0E77F015947837171a968";
   // Set sale enable
+  IERC20(tAddress).approve
   await execute(
     'AneroToken',
     {from: deployer.address, log: true},
-    'withdrawMoney',
+    'withdrawToken',
+    "0xc7D4796B43272A329Ba0E77F015947837171a968",
     "0x1ecF212beaA83E1E892fDc8E579C91Cd8D0102A2",
     utils.parseEther('40')
   )
 };
 fn.skip = async (hre) => {
-  return true;
+  return false;
   // Skip this on kovan.
   const chain = parseInt(await hre.getChainId());
   return chain != 1;

df209c1bbc043c51de0952bf85d3a13084196129
trust0212 <sergeyfedorenkodev@outlook.com>
2022-06-19 16:05:06 -0400

    complete smart contract

--- contracts/Airdrop.sol
+++ contracts/Airdrop.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "./ERC721A.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 interface ERC721Partial {
@@ -18,6 +16,7 @@ contract Airdrop is Ownable {
     address signer;
     address treasury;
     mapping(address => bool) public claimed;
+
     constructor(ERC721Partial _nft, address _signer) {
         nft = _nft;
         signer = _signer;
@@ -25,24 +24,23 @@ contract Airdrop is Ownable {
     }
 
     /// @notice Tokens on the given ERC-721 contract are transferred from you to a recipient.
-    ///         Don't forget to execute setApprovalForAll first to authorize this contract.
     /// @param  amount      Which token IDs are transferred?
-    function claim(uint16 amount, bytes signature) external {
-        require(verifySigner(signature, sigher), "Invalid signature."); 
+    function claim(uint16 amount, bytes calldata signature) external {
+        require(verifySigner(signature), "Invalid signature."); 
         require(totalAirdropCount + amount <= 1858, "Exceeds claim amount");
         require(!claimed[msg.sender], "Already claimed.");
 
         for (uint16 index = 0; index < amount; index ++) {
-            uint16 tokenId = nft.tokenOfOwnerByIndex(treasury, totalAirdropCount + index);
-            tokenContract.transferFrom(treasury,msg.sender, tokenId);
+            uint256 tokenId = nft.tokenOfOwnerByIndex(treasury, index);
+            nft.transferFrom(treasury,msg.sender, tokenId);
         }
         claimed[msg.sender] = true;
         totalAirdropCount += amount;
     }
 
     //
-    function verifySigner(bytes calldata signature, address signer) 
-        public view returns (bool) {
+    function verifySigner(bytes calldata signature) 
+        internal view returns (bool) {
         bytes32 hash = keccak256(abi.encodePacked(msg.sender));
         bytes32 message = ECDSA.toEthSignedMessageHash(hash);
         address recoveredAddress = ECDSA.recover(message, signature);
--- deploy/00_deploy.ts
+++ deploy/00_deploy.ts
@@ -6,14 +6,15 @@ const fn: DeployFunction = async function ({ deployments: { deploy }, ethers: { 
 
   // const nftAddress = "0x926baA7445D56D5E8632046409cDC7d5844CE344"; // mainnet
   const nftAddress = "0xd7CE44F48F6c239a70943C9E7AaA2Db31c05ECD2";
-  const signer = 
+  const signer = "0x5e6cCfBa0aB8Bf8BDEE5ABe9f6eE0BB2f274a609";
 
   const contractDeployed = await deploy('Airdrop', {
     from: deployer.address,
     log: true,
     skipIfAlreadyDeployed: false,
     args: [
-      nftAddress
+      nftAddress,
+      signer
     ]
   });
 

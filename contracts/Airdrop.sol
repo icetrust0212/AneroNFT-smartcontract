ce12987adbad8ea547c4905c411c7e4b4edbf620
trust0212 <sergeyfedorenkodev@outlook.com>
2022-06-19 11:13:41 -0400

    contract

--- contracts/Airdrop.sol
+++ contracts/Airdrop.sol
@@ -1,29 +1,59 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "./ERC721A.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 interface ERC721Partial {
     function transferFrom(address from, address to, uint256 tokenId) external;
     function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256);
 }
 contract Airdrop is Ownable {
+    using Strings for uint256;
+    using ECDSA for bytes32;
 
     ERC721Partial public nft;
     uint16 public totalAirdropCount;
-
-    constructor(ERC721Partial _nft, ) {
+    address signer;
+    address treasury;
+    mapping(address => bool) public claimed;
+    constructor(ERC721Partial _nft, address _signer) {
         nft = _nft;
+        signer = _signer;
+        treasury = msg.sender;
     }
 
     /// @notice Tokens on the given ERC-721 contract are transferred from you to a recipient.
     ///         Don't forget to execute setApprovalForAll first to authorize this contract.
-    /// @param  recipient     Who gets the tokens?
     /// @param  amount      Which token IDs are transferred?
-    function batchTransfer(address recipient, uint16 amount, bytes signature) external {
-        for (uint16 index; index < amount; index++) {
-            uint16 tokenId = nft.tokenOfOwnerByIndex(owner, index);
-            tokenContract.transferFrom(msg.sender, recipient, tokenIds[index]);
-        }
+    function claim(uint16 amount, bytes signature) external {
+        require(verifySigner(signature, sigher), "Invalid signature."); 
+        require(totalAirdropCount + amount <= 1858, "Exceeds claim amount");
+        require(!claimed[msg.sender], "Already claimed.");
 
+        for (uint16 index = 0; index < amount; index ++) {
+            uint16 tokenId = nft.tokenOfOwnerByIndex(treasury, totalAirdropCount + index);
+            tokenContract.transferFrom(treasury,msg.sender, tokenId);
+        }
+        claimed[msg.sender] = true;
+        totalAirdropCount += amount;
+    }
+
+    //
+    function verifySigner(bytes calldata signature, address signer) 
+        public view returns (bool) {
+        bytes32 hash = keccak256(abi.encodePacked(msg.sender));
+        bytes32 message = ECDSA.toEthSignedMessageHash(hash);
+        address recoveredAddress = ECDSA.recover(message, signature);
+        return (recoveredAddress != address(0) && recoveredAddress == signer);
+    }
+
+    function setSigner(address _signer) external onlyOwner {
+        signer = _signer;
+    }
+
+    function setTreasury(address _treasury) external onlyOwner {
+        treasury = _treasury;
     }
 }
--- deploy/00_deploy.ts
+++ deploy/00_deploy.ts
@@ -6,6 +6,7 @@ const fn: DeployFunction = async function ({ deployments: { deploy }, ethers: { 
 
   // const nftAddress = "0x926baA7445D56D5E8632046409cDC7d5844CE344"; // mainnet
   const nftAddress = "0xd7CE44F48F6c239a70943C9E7AaA2Db31c05ECD2";
+  const signer = 
 
   const contractDeployed = await deploy('Airdrop', {
     from: deployer.address,

ceae68e45048809cc41909ea6e15cb4fc279ace8
trust0212 <sergeyfedorenkodev@outlook.com>
2022-06-19 01:04:06 -0400

    a

--- contracts/Airdrop.sol
+++ contracts/Airdrop.sol
@@ -1,38 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 interface ERC721Partial {
     function transferFrom(address from, address to, uint256 tokenId) external;
     function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256);
 }
-contract Airdrop {
-    struct Data {
-        address receipent;
-        uint256 amount;
-    }
+contract Airdrop is Ownable {
 
     ERC721Partial public nft;
-    uint airdropedAmount;
+    uint16 public totalAirdropCount;
 
-    constructor(ERC721Partial _nft) {
+    constructor(ERC721Partial _nft, ) {
         nft = _nft;
     }
 
-    function batchTransfer(Data[] memory data) external {
-        for (uint i = 0 ; i < data.length; i ++) {
-            _transfer(data[i].receipent, data[i].amount);
-            airdropedAmount += data[i].amount;
+    /// @notice Tokens on the given ERC-721 contract are transferred from you to a recipient.
+    ///         Don't forget to execute setApprovalForAll first to authorize this contract.
+    /// @param  recipient     Who gets the tokens?
+    /// @param  amount      Which token IDs are transferred?
+    function batchTransfer(address recipient, uint16 amount, bytes signature) external {
+        for (uint16 index; index < amount; index++) {
+            uint16 tokenId = nft.tokenOfOwnerByIndex(owner, index);
+            tokenContract.transferFrom(msg.sender, recipient, tokenIds[index]);
         }
-    }
 
-    function _transfer(address receipent, uint amount) internal {
-        for (uint i = 0; i < amount; i ++) {
-            uint tokenId = nft.tokenOfOwnerByIndex(msg.sender, airdropedAmount + i);
-            nft.transferFrom(msg.sender, receipent, tokenId);
-        }
-    }
-
-    function setNFT(ERC721Partial _nft) external onlyOwner {
-        
     }
 }
